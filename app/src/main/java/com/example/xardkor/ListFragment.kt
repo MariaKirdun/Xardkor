e6ed842dfe1f528e9eae427f2baba47101aa2947
Maria Kirdun <manya.gumil@gmail.com>
2020-03-25 23:17:17 +0300

    add listener, adapter and recycler

--- app/src/main/java/com/example/xardkor/ListFragment.kt
+++ app/src/main/java/com/example/xardkor/ListFragment.kt
@@ -4,9 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.list_fragment.*
+import kotlinx.android.synthetic.main.list_item.*
+import kotlinx.android.synthetic.main.list_item.view.*
 
 class ListFragment: Fragment() {
+
+    private lateinit var adapter: MapPointsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,4 +24,33 @@ class ListFragment: Fragment() {
         return inflater.inflate(R.layout.list_fragment, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        adapter = MapPointsAdapter()
+        recycler.adapter = adapter
+        recycler.layoutManager = LinearLayoutManager(this.context)
+
+        adapter.dataset = listOf("Absolut", "Beee", "cxsfC")
+        adapter.notifyDataSetChanged()
+    }
+
+    class MapPointsAdapter() :
+            RecyclerView.Adapter<MapPointsAdapter.MapPointViewHolder>() {
+
+        var dataset: List<String> = listOf()
+
+        class MapPointViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MapPointViewHolder {
+            return MapPointViewHolder(
+                LayoutInflater.from(parent.context)
+                .inflate(R.layout.list_item, parent, false)
+            )
+        }
+
+        override fun getItemCount(): Int = dataset.size
+
+        override fun onBindViewHolder(holder: MapPointViewHolder, position: Int) {
+            holder.view.textView.text = dataset[position]
+        }
+    }
 }
